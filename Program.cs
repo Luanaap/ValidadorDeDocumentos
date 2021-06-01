e49f9ba8a873a4fef5a31bb2344c4c2664bc33f3
Luanaap <l.andrade2711@gmail.com>
2021-05-31 21:19:36 -0300

    ValidadorDeDocumentos aula 1 e aula 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Caelum.Stella.CSharp.Validation;
+﻿using Caelum.Stella.CSharp.Format;
+using Caelum.Stella.CSharp.Validation;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -18,18 +19,65 @@ namespace ValidadorDeDocumentos
             ValidarCPF(cpf1);
             ValidarCPF(cpf2);
             ValidarCPF(cpf3);
+
+            string cnpj1 = "68619111000114";
+            string cnpj2 = "43866377000130";
+
+            ValidarCNPJ(cnpj1);
+            ValidarCNPJ(cnpj2);
+
+            string titulo1 = "265533210108";
+            string titulo2 = "053566430124";
+
+            ValidarTitulo(titulo1);
+            ValidarTitulo(titulo2);
+
+            Debug.WriteLine(cpf1);
+            string cpfFormatado = new CPFFormatter().Format(cpf1);
+            Debug.WriteLine(cpfFormatado);
+            Debug.WriteLine(new CPFFormatter().Format(cpfFormatado));
+            Debug.WriteLine(new CPFFormatter().Unformat(cpfFormatado));
+
+            Debug.WriteLine(cnpj1);
+            Debug.WriteLine(new CNPJFormatter().Format(cnpj1));
+
+            Debug.WriteLine(titulo1);
+            Debug.WriteLine(new TituloEleitoralFormatter().Format(titulo1));
+        }
+
+        private static void ValidarTitulo(string titulo)
+        {
+            if (new TituloEleitoralValidator().IsValid(titulo))
+            {
+                Debug.WriteLine("Titulo válido: " + titulo);
+            }
+            else
+            {
+                Debug.WriteLine("Titulo inválido: " + titulo);
+            }
+        }
+
+        private static void ValidarCNPJ(string cnpj)
+        {
+            if (new CNPJValidator().IsValid(cnpj))
+            {
+                Debug.WriteLine("CNPJ válido: " + cnpj);
+            }
+            else
+            {
+                Debug.WriteLine("CNPJ inválido: " + cnpj);
+            }
         }
 
         private static void ValidarCPF(string cpf)
         {
-            try
-            {
-                new CPFValidator().AssertValid(cpf);
+            if (new CPFValidator().IsValid(cpf))
+            { 
                 Debug.WriteLine("CPF válido: " + cpf);
             }
-            catch (Exception exc)
+            else
             {
-                Debug.WriteLine("CPF inválido: " + cpf + " : " + exc.ToString());
+                Debug.WriteLine("CPF inválido: " + cpf);
             }
         }
     }
